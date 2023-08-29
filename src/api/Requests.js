0e21042d770df01650776779dcb058495965fba8
yoonki <yoonkeee@gmail.com>
2023-08-29 20:10:33 +0900

    feat: data load 실패 대비 try-catch 추가, 데이터 체크 로직 추가

--- src/api/Requests.js
+++ src/api/Requests.js
@@ -2,26 +2,53 @@ async function articlesData() {
   let articlesList;
   let articles;
 
-  await axios.get("../../data/articlesList.json").then((res) => {
-    articlesList = res.data;
-  });
-  await axios.get("../../data/articles.json").then((res) => {
-    articles = res.data;
-  });
+  const loadData = async function () {
+    try {
+      await axios.get("../../data/articlesList.json").then((res) => {
+        articlesList = res.data;
+      });
+    } catch (e) {
+      alert("Error at Requests.js : " + e);
+    }
 
-  let getArticlesList = function () {
+    try {
+      await axios.get("../../data/articles.json").then((res) => {
+        articles = res.data;
+      });
+    } catch (e) {
+      alert("Error at Requests.js: " + e);
+    }
+  }
+
+  await loadData();
+
+  const checkDataExist = async function () {
+    if (articlesList === undefined || articles === undefined) {
+      await loadData();
+      return false;
+    }
+    return true;
+  }
+
+  const getArticlesList = function () {
+    if (!checkDataExist()) {
+      return;
+    }
     return articlesList;
   };
-  let getArticle = function (id) {
+  const getArticle = function (id) {
+    if (!checkDataExist()) {
+      return;
+    }
     return articles["article_id"][id];
   };
-  return { getArticlesList, getArticle };
+  return {getArticlesList, getArticle};
 }
 
-let data = await articlesData();
+const data = await articlesData();
 
 
-export default function Requests () {
+export default function Requests() {
   return {
     getArticlesList: data.getArticlesList,
     getArticle: data.getArticle
