3496d638e35a32a26905c777176d6fd6903d8e7f
yoonki <yoonkeee@gmail.com>
2023-08-29 17:13:42 +0900

    feat: App.js inline-css 제거, index.css에 클래스로 추가

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 const app = document.getElementById("root");
 app.innerHTML = /* HTML */ `
   <section>
-    <header style="height: 60px; background-color: white; border-bottom: 1px solid black; position: fixed; top: 0; left: 0; right: 0;">
-      <div style="width: 100%; max-width: 1140px; height: 100%; margin: 0 auto">
-        <div style="display: flex; height: 100%;">
-          <div style="display: flex; align-items: center" id="logoImage">
-            <img src="images/LogoImage.png" style="max-height: 50px" alt="로고 이미지" />
-          </div>
+    <header class="main-header">
+      <div class="header-container">
+        <div id="logoImage">
+          <img src="images/LogoImage.png" class="logo-image" alt="로고 이미지" />
         </div>
       </div>
     </header>
   </section>
   <section>
     <div>
-      <div id="content" style="min-height: calc(100vh - 60px); margin: 61px auto 50px;"></div>
+      <div id="content" class="content"></div>
     </div>
   </section>
 `;
@@ -21,5 +19,5 @@ app.innerHTML = /* HTML */ `
 document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("logoImage").addEventListener("click", () => {
     history.pushState(null, null, "/");
-  })
+  });
 });
