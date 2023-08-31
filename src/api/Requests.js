e086350cdd7cc3f54efd34cc1cfadf1d5de3aa25
Yoonkeee <yoonkeee@gmail.com>
2023-08-31 18:43:15 +0900

    feat: 메인 페이지 렌더에 필요한 데이터를 불러오는 모듈 생성

---
+++ src/api/Requests.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+async function articlesData() {
+  let articlesList;
+
+  const loadData = async function () {
+    try {
+      await axios.get("../../data/articlesList.json").then((res) => {
+        articlesList = res.data;
+      });
+    } catch (e) {
+      alert("Error at Requests.js : " + e);
+    }
+
+    await loadData();
+
+    const checkDataExist = async function () {
+      if (articlesList === undefined) {
+        await loadData();
+        return false;
+      }
+      return true;
+    };
+
+    const getArticlesList = function () {
+      if (!checkDataExist()) {
+        return;
+      }
+      return articlesList;
+    };
+
+    return { getArticlesList };
+  };
+}
+
+const data = await articlesData();
+
+export default function Requests() {
+  return {
+    getArticlesList: data.getArticlesList,
+  };
+}
