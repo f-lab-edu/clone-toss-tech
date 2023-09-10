c119bfc029f7ef3ef12fbdbf95134ac9875821f4
Yoonkeee <yoonkeee@gmail.com>
2023-09-10 23:11:24 +0900

    feat: getArticleListHandler 단축

--- src/mocks/get-article-list.js
+++ src/mocks/get-article-list.js
@@ -2,8 +2,6 @@ import { rest } from 'msw';
 import { createMockServerEndpoint } from './config';
 import { fetchFile } from '../utils';
 
-const getArticleListHandler = () => {
+export default function getArticleListHandler() {
   return createMockServerEndpoint(rest.get, '/api/article-list', fetchFile('article-list'), (req, data) => data);
-};
-
-export default getArticleListHandler;
+}
