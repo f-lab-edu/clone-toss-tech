089da143dde5f70ed3d5c2a7714892ea11f6627a
Yoonkeee <yoonkeee@gmail.com>
2023-09-13 22:11:30 +0900

    feat: 개발 환경에서만 console.error 남기도록 수정

--- src/mocks/index.js
+++ src/mocks/index.js
@@ -8,8 +8,9 @@ const initMocks = () => {
     const worker = setupWorker(...mockHandlers);
     return worker.start();
   } catch (e) {
-    // TODO: .env에서 개발/배포 환경 변수 불러와서 개발일 경우만 콘솔 남기도록 구분할 예정 Ticket WOO-64
-    console.error(`Failed to start worker. Error is : ${e}`);
+    if (import.meta.env.VITE_APP_ENV === 'development') {
+      console.error(`Failed to start worker. Error is : ${e}`);
+    }
     return null;
   }
 };
