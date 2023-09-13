e39b5400c17ecf630f6382d4b41705b186972935
Yoonkeee <yoonkeee@gmail.com>
2023-09-13 23:33:03 +0900

    feat: 개발 환경에서만 console.error 남기도록 수정

--- src/api/index.js
+++ src/api/index.js
@@ -8,8 +8,9 @@ export const fetchData = path => {
   try {
     return instance.get(path);
   } catch (e) {
-    // TODO: .env에서 개발/배포 환경 변수 불러와서 개발일 경우만 콘솔 남기도록 구분할 예정 Ticket WOO-64
-    console.error(`Error at api/index.js : ${e}`);
+    if (import.meta.env.VITE_APP_ENV === 'development') {
+      console.error(`Error at api/index.js : ${e}`);
+    }
     return null;
   }
 };
