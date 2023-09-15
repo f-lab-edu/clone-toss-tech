f2f433743672b26f3e041469e76e224438289fdf
Yoonkeee <yoonkeee@gmail.com>
2023-09-15 15:23:58 +0900

    feat: 개발 환경에서만 console.error 남기도록 built-in 환경 변수 사용 (#20)

--- src/mocks/utils.js
+++ src/mocks/utils.js
@@ -15,8 +15,9 @@ export function fetchMocks(path) {
       }
     }
   } catch (e) {
-    // TODO: .env에서 개발/배포 환경 변수 불러와서 개발일 경우만 콘솔 남기도록 구분할 예정 Ticket WOO-64
-    console.error(`Error at utils.js : ${e}`);
+    if (import.meta.env.DEV) {
+      console.error(`Error at mocks/utils.js : ${e}`);
+    }
     return null;
   }
 }
