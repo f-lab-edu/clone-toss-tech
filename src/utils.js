0656d1a167c4be379b500b33461f1a5768a4c3ce
Yoonkeee <yoonkeee@gmail.com>
2023-09-19 11:05:22 +0900

    feat: 메인 페이지 렌더를 위한 함수 생성

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,9 @@
 export const html = (strings, ...values) => String.raw({ raw: strings }, ...values);
+
+export function formatDate(inputDate) {
+  const year = inputDate.substring(0, 4);
+  const month = inputDate.substring(4, 6);
+  const day = inputDate.substring(6, 8);
+
+  return `${year}. ${parseInt(month, 10)}. ${parseInt(day, 10)}`;
+}
---
+++ src/views/main/index.js
@@ -0,0 +1,37 @@
+import { formatDate, html } from '../../utils';
+
+const setupTemplate = () => {
+  const $content = document.getElementById('content');
+  $content.innerHTML = html`
+    <div id="articles">
+      <h1>메인 페이지</h1>
+    </div>
+  `;
+};
+
+const makeArticleElement = props => {
+  const { thumbnail_image: thumbnailImage, title, summary, created_date: createdDate, id } = props;
+  const article = document.createElement('div');
+  article.id = id;
+  article.innerHTML = html`
+    <img alt="썸네일 이미지" src="${thumbnailImage}" />
+    <div>
+      <span>${title}</span>
+      <span>${summary}</span>
+      <span>${formatDate(createdDate)}</span>
+    </div>
+  `;
+  return article;
+};
+
+export default function render(articleList) {
+  setupTemplate();
+  const newArticlesUlElement = document.createElement('ul');
+  const articleListArray = Object.entries(articleList).map(([id, value]) => ({ id, ...value }));
+  const $articles = document.getElementById('articles');
+
+  Object.keys(articleList).forEach(() => {
+    newArticlesUlElement.append(makeArticleElement(articleListArray.pop()));
+  });
+  $articles.appendChild(newArticlesUlElement);
+}
