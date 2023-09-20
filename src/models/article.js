15ac13c01cbd588ed172d7df78027577323fc894
Yoonkeee <yoonkeee@gmail.com>
2023-09-20 19:55:09 +0900

    feat: setArticleList 함수 수정

--- src/models/article.js
+++ src/models/article.js
@@ -1,26 +1,39 @@
 import { fetchData } from '../api/index.js';
 
-const makeRequestToApi = path => {
+const makeRequestToAPI = path => {
   try {
     return fetchData(path).then(res => res.data);
   } catch (e) {
     if (import.meta.env.DEV) {
-      console.error(`Error at article-data-handler.js makeRequestToApi(${path} : ${e}`);
+      console.error(`Error at article.js makeRequestToAPI(${path} : ${e}`);
     }
     return null;
   }
 };
 
-const setArticleList = async () => await makeRequestToApi('/articles');
+const setArticleList = async () => {
+  const result = await makeRequestToAPI('/articles');
+  try {
+    if (!!result && !!result.articles) {
+      return result.articles;
+    }
+    throw new Error('articleList is not defined.');
+  } catch (e) {
+    if (import.meta.env.DEV) {
+      console.error(`Error at article.js setArticleList() : ${e}`);
+    }
+    return null;
+  }
+};
 
 function Article() {
   this.articleList = setArticleList();
   this.getArticleList = async () => await this.articleList;
 
   this.getArticleBody = async id => {
-    const body = await makeRequestToApi(`/article/${id}`);
+    const body = await makeRequestToAPI(`/article/${id}`);
     return {
-      ...this.articleList.articles[id],
+      ...this.articleList[id],
       body: body.data,
     };
   };
