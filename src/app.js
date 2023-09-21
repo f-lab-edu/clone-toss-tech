451902b32cce0275b6f86d50ec6a24f4f5aa3b00
Yoonkeee <yoonkeee@gmail.com>
2023-09-21 17:59:24 +0900

    feat: 앱에서 데이터를 다루기 위해 Mock server init, models 디렉토리 및 소스 생성 (#22)

--- src/app.js
+++ src/app.js
@@ -1,17 +1,25 @@
 import { html } from './utils';
+import { initMocks } from './mocks/index';
 
-const app = document.getElementById('root');
-app.innerHTML = html`
-  <section>
-    <header class="main-header">
-      <div class="header-container">
-        <div class="logo-image">
-          <img alt="로고 이미지" src="/images/logo-image.png" />
+const hasMocks = initMocks();
+const start = () => {
+  const app = document.getElementById('root');
+  app.innerHTML = html`
+    <section>
+      <header class="main-header">
+        <div class="header-container">
+          <div class="logo-image">
+            <img alt="로고 이미지" src="/images/logo-image.png" />
+          </div>
         </div>
-      </div>
-    </header>
-  </section>
-  <section>
-    <div class="content" id="content"></div>
-  </section>
-`;
+      </header>
+    </section>
+    <section>
+      <div class="content" id="content"></div>
+    </section>
+  `;
+};
+
+if (hasMocks) {
+  document.addEventListener('DOMContentLoaded', start);
+}
---
+++ src/models/article.js
@@ -0,0 +1,34 @@
+import { fetchData } from '../api/index.js';
+
+const makeRequest = path => {
+  try {
+    return fetchData(path).then(res => res.data);
+  } catch (e) {
+    // TODO: DEV 환경일 때 에러 정보 로그를 출력하는 Util function 추가 예정 Ticket: WOO-75
+    if (import.meta.env.DEV) console.error(`Error at article.js makeRequestToAPI(${path} : ${e}`);
+    return null;
+  }
+};
+
+const setArticleList = async () => {
+  const result = await makeRequest('/articles');
+  if (!result) return null;
+  return result.articles;
+};
+
+function Article() {
+  this.articleList = setArticleList();
+  this.getArticleList = async () => await this.articleList;
+  this.getArticleBody = async id => {
+    try {
+      const { data } = await makeRequest(`/article/${id}`);
+      if (data && this.articleList[id]) return { ...this.articleList[id], body: data };
+    } catch (e) {
+      // TODO: 상동 Ticket: WOO-75
+      if (import.meta.env.DEV) console.error(`Error at article.js getArticleBody(${id}) : ${e}`);
+      return null;
+    }
+  };
+}
+
+export default Article;
