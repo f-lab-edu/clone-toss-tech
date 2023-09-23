8ea0a3c01bc01c395c55f8924722166e7b0a97e7
Yoonkeee <yoonkeee@gmail.com>
2023-09-24 00:13:38 +0900

    feat: 개발 환경에서 console.error 정보를 기록하는 유틸 함수 추가

--- src/utils.js
+++ src/utils.js
@@ -3,3 +3,12 @@ import { format, parseISO } from 'date-fns';
 export const html = (strings, ...values) => String.raw({ raw: strings }, ...values);
 
 export const formatDate = inputDate => format(parseISO(inputDate), 'yyyy.MM.dd');
+
+export const makeErrorLogOnConsole = (error, params) => {
+  if (import.meta.env.DEV) {
+    const stackLines = new Error().stack.split('\n')[2];
+    const fileName = stackLines.match(/(\w*:\/\/\w*:\d+)(.*)\?.*/)[2];
+    const functionName = stackLines.match(/at (\S+)/)[1];
+    console.error(`Error at ${fileName} function ${functionName}(${params ?? ''}) ${error}`);
+  }
+};
