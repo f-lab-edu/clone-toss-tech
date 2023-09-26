29038b9bf5d7c5f6c72fef56268678c866df8475
Yoonkeee <yoonkeee@gmail.com>
2023-09-26 23:22:26 +0900

    feat: targetID에 대한 예외 처리 추가

--- src/controllers/article.js
+++ src/controllers/article.js
@@ -3,9 +3,13 @@ import renderArticlePage from '../views/article/index';
 function Article(router, model) {
   this.render = async () => {
     try {
-      const targetID = window.location.pathname.replace('/article/', '').match(/\d+/).toString();
-      const data = await model.getArticleBody(targetID);
-      renderArticlePage(data);
+      const match = window.location.pathname.match(/\/article\/(\d+)/);
+      const targetID = match ? match[1] : null;
+      if (targetID) {
+        const data = await model.getArticleBody(targetID);
+        renderArticlePage(data);
+      }
+      return null;
     } catch (e) {
       if (import.meta.env.DEV) console.error(`Error at controllers/article.js render() : ${e}`);
     }
