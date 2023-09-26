580996efab5486fd45bcf61e480f248e9939f3a5
Yoonkeee <yoonkeee@gmail.com>
2023-09-26 11:19:02 +0900

    feat: main 페이지를 담당하는 컨트롤러 생성 (#27)

---
+++ src/controllers/main.js
@@ -0,0 +1,27 @@
+import renderMainPage from '../views/main/index';
+
+const setClickEventListenerOnArticle = router => {
+  const $articleHeadContainers = document.querySelectorAll('li[class*="main-article"]');
+  $articleHeadContainers.forEach($li => {
+    $li.addEventListener('click', () => {
+      window.history.pushState('', '', `/article/${$li.id}`);
+      router.navigate(window.location.pathname);
+      window.scrollTo(0, 0);
+    });
+  });
+};
+
+function Main(router, model) {
+  this.render = async () => {
+    try {
+      const contents = await model.getArticleList();
+      renderMainPage(contents);
+      setClickEventListenerOnArticle(router);
+    } catch (e) {
+      if (import.meta.env.DEV) console.error(`Error at controllers/main.js render : ${e}`);
+    }
+  };
+  router.addRoute('/', this.render.bind(this));
+}
+
+export default Main;
